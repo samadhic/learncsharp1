41a6746176943bf1ad65967b1df9122b9d5198fc
Samadhi Chandrasekara <samadhichandrasekara2004@gmail.com>
2024-02-07 21:50:08 +0530

    my  first console app

---
+++ myconsole/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace myconsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, what's your name?");
+        string myName= Console.ReadLine();
+        Console.WriteLine($"Hello, {myName}!");
+    }
+}
